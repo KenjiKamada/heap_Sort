77b58a0de953291e17cd3f0c4d8d2e9c32a6e678
Kenji Kamada <kamadak@kawai.co.jp>
2022-04-11 11:05:54 +0900

    完成

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,85 @@
 //use std::collections::BinaryHeap;
 //use std::mem;
 //use std::cmp::Ordering;
-//use rand::Rng;
+use rand::Rng;
 //use std::f64;
 
 // https://daeudaeu.com/heap-sort/
 fn main() {
-//    const N:usize = 5; //N個の整数数列
-   // let mut x: [i32; N]=[0;N]; //乱数
-        // -1000 以上 1000 以下のランダムな整数を100個用意する。
-        // for n in 1..x.len(){
-        //     x[n] = rand::thread_rng().gen_range(-1000, 1000+1);
-        // }
+    const N:usize = 100; //N個の整数数列
 
+   // 乱数生成 
+   //  let mut x0=vec![5,3,9,10,34,7,6,3,4,66,3,1,7,9,3];
+   let  mut x0 = Vec::with_capacity(N);
+   x0.resize(N, 0); // https://doc.rust-lang.org/std/vec/struct.Vec.html
+       // -1000 以上 1000 以下のランダムな整数を100個用意する。
+        for n in 0..N{
+            x0[n] = rand::thread_rng().gen_range(-1000, 1000+1);
+        }
 
-
-
-
-    let mut x0=vec![5,3,9,10,34,7,6,3,4,66,3,1,7,9,3];
-    let mut x=vec![0;100];
-
-    for n in 0..x0.len(){
-            x[n] = x0[n];
-
-    }
-   // x=[5,0,9,7,1,6,3,8,4];
-
+        
     let _nx:f32=x0.len() as f32;
-    let nx = _nx as usize;
     let kf = _nx.log2()+1.0;
+    let nx = _nx as usize;
     let k = kf.floor();
-    let mut L = Vec::<i32>::new(); // メモリ上の階層位置
+    let mut el = Vec::<i32>::new(); // メモリ上の階層位置
 
-    //    L[2] = 1;
-
-    
-             for n in 0..k as i32{ 
-                 let mut m ;
-                 let two:i32 =2;
-                 
-                 m = two.pow(n.try_into().unwrap());
-                 L.push(m as i32);
-             }
-
-            for n in 0..x0.len(){
-                println!("ソート前={}",x[n]);
+            // el = [1,2,4,8,16,....]を作る。
+            for n in 0..k as i32{ 
+                let m ;
+                let two:i32 =2;
+                
+                m = two.pow(n.try_into().unwrap());
+                el.push(m as i32);
             }
-    
-           // x.swap(1, 2);
-         
-           heap_sort(&mut x, &L, nx);
+  
+    let el_iter = el.iter();
+    let _n_all:i32 = el_iter.sum();
+    let n_all:usize = _n_all as usize;
+    let mut x = Vec::with_capacity(n_all); 
 
-            for n in 0..x0.len(){
-                println!("後={}",x[n]);
-            }
-    
+        x.resize(n_all, 0);  // アロケーション
+        x[0..N].copy_from_slice(&x0[0..N]); // https://stackoverflow.com/questions/66609964/rust-looking-for-a-c-memcpy-equivalent
+            
+              heap_sort(&mut x, &el, nx); // ヒープソートの関数
 
-       // let mut heap:[i32];
-
-
+    // 確認  yにソート結果
+    // 全部"True！"なら良い。
+    for n in 0..x0.len(){
+        println!("sort前={}, sort結果={}", x0[n], x[n]);
+        assert!(x[n] >= x[n+1]);
+    }
 
 }
 
-fn heap_sort(x:&mut Vec<i32>, L:&Vec<i32>, nx:usize){
-   // x.swap(1, 2);
+////////////////////////////////////////////////////////////////
+fn heap_sort(x:&mut Vec<i32>, el:&Vec<i32>, nx:usize){
    
    let mut x_result = vec![0;nx];
 
    for u in 0..nx{
-        for l in {1..L.len()}.rev(){
+        for l in {1..el.len()}.rev(){
         
-            for k in {0..L[l-1]}{
+            for k in 0..el[l-1]{
                 let m=2*k;
                 let n=m+1;
 
-                if x[(L[l]+m-1) as usize] < x[(L[l]+n-1) as usize]{ // 左が大きいにする
-                    x.swap((L[l]+m-1) as usize, (L[l]+n-1) as usize);
+                if x[(el[l]+m-1) as usize] < x[(el[l]+n-1) as usize]{ // 左が大きいにする
+                    x.swap((el[l]+m-1) as usize, (el[l]+n-1) as usize);
                 }
 
-                if x[(L[l-1]+k-1) as usize] < x[(L[l]+m-1) as usize]{ // 上が大きいにする。
-                    x.swap((L[l-1]+k-1) as usize, (L[l]+m-1) as usize);
+                if x[(el[l-1]+k-1) as usize] < x[(el[l]+m-1) as usize]{ // 上が大きいにする。
+                    x.swap((el[l-1]+k-1) as usize, (el[l]+m-1) as usize);
                 }
             }
         }
 
         x_result[u] = x[0];
-        x[0] = -1;
-        println!("x_result={}",x_result[u]);
+        x[0] = std::i32::MIN; // 最小値https://qiita.com/mkimura81/items/03f2ed423905f6b39935
     }
 
-        for i in 0..nx{
-            x[i] = x_result[i];
-        }
-
+        // x_result -> xにnx個コピーする。 
+        x[0..nx].copy_from_slice(&x_result[0..nx]); // https://stackoverflow.com/questions/66609964/rust-looking-for-a-c-memcpy-equivalent  
     }
 
 
