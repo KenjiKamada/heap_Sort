7a92631de43edecca33f6b92799e2c0ccaefa2c1
Kenji Kamada <kamadak@kawai.co.jp>
2022-04-15 09:29:08 +0900

    修正 負値対応

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use rand::Rng;
 fn main() {
     const N:usize = 100; //N個の整数数列
 
+
    // 乱数生成 
-   //  let mut x0=vec![5,3,9,10,34,7,6,3,4,66,3,1,7,9,3];
+//    let mut x0=vec![-5,3,-9,10];
    let  mut x0 = Vec::with_capacity(N);
    x0.resize(N, 0); // https://doc.rust-lang.org/std/vec/struct.Vec.html
        // -1000 以上 1000 以下のランダムな整数を100個用意する。
@@ -32,22 +33,24 @@ fn main() {
     let el_iter = el.iter();
     let _n_all:i32 = el_iter.sum();
     let n_all:usize = _n_all as usize;
-    let mut x = Vec::with_capacity(n_all); 
+    let mut x = Vec::with_capacity(nx+1); 
 
-        x.resize(n_all, 0);  // アロケーション
+        x.resize(nx+1, std::i32::MIN);  // アロケーション
         x[0..N].copy_from_slice(&x0[0..N]); // https://stackoverflow.com/questions/66609964/rust-looking-for-a-c-memcpy-equivalent
             
               heap_sort(&mut x, &el, nx); // ヒープソートの関数
 
     // 確認  yにソート結果
-    // 全部"True！"なら良い。
     for n in 0..x0.len(){
         println!("sort前={}, sort結果={}", x0[n], x[n]);
+    }
+
+    // 全部"True！"なら良い。
+    for n in 0..x0.len()-1{
         assert!(x[n] >= x[n+1]);
     }
 
 }
-
 ////////////////////////////////////////////////////////////////
 fn heap_sort(x:&mut Vec<i32>, el:&Vec<i32>, nx:usize){
    
@@ -55,28 +58,25 @@ fn heap_sort(x:&mut Vec<i32>, el:&Vec<i32>, nx:usize){
 
    for u in 0..nx{
         for l in {1..el.len()}.rev(){
-        
             for k in 0..el[l-1]{
-                let m=2*k;
-                let n=m+1;
-
+              let m=2*k;
+              let n=m+1;
+                if (el[l]+n-1) > nx as i32 {break;} // xの個数以上はやらない
                 if x[(el[l]+m-1) as usize] < x[(el[l]+n-1) as usize]{ // 左が大きいにする
                     x.swap((el[l]+m-1) as usize, (el[l]+n-1) as usize);
                 }
-
                 if x[(el[l-1]+k-1) as usize] < x[(el[l]+m-1) as usize]{ // 上が大きいにする。
                     x.swap((el[l-1]+k-1) as usize, (el[l]+m-1) as usize);
                 }
             }
         }
-
         x_result[u] = x[0];
         x[0] = std::i32::MIN; // 最小値https://qiita.com/mkimura81/items/03f2ed423905f6b39935
     }
-
         // x_result -> xにnx個コピーする。 
         x[0..nx].copy_from_slice(&x_result[0..nx]); // https://stackoverflow.com/questions/66609964/rust-looking-for-a-c-memcpy-equivalent  
-    }
+
+} // end of heap_sort()
 
 
 
--- target/debug/heap_sort.d
+++ target/debug/heap_sort.d
@@ -1,1 +1,1 @@
-C:\Users\257788\OneDrive\ -\ 株式会社河合楽器製作所\Rust\heap_Sort\target\debug\heap_sort.exe: C:\Users\257788\OneDrive\ -\ 株式会社河合楽器製作所\Rust\heap_Sort\src\main.rs
+O:\Rust\heap_Sort\target\debug\heap_sort.exe: O:\Rust\heap_Sort\src\main.rs
